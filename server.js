a9578c756220a8d7556813a4f3b2d127e134b1a1
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-07-24 22:25:46 -0400

    view routes added and filePath created

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
 const PORT = process.env.PORT || 3000;
-
 const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
@@ -14,48 +11,11 @@ app.use(express.urlencoded({ extended: true }, { useUnifiedTopology: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// Routes
+const routes = require("./controllers/workoutController.js");
+app.use(routes);
 
-// routes
-// "/api/workouts"
-app.get("/api/workouts", (_, res) => {
-    db.Workout.find({})
-      .then(dbWorkout => {
-          console.log(dbWorkout);
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-// "/api/workouts/:id" 
-app.get("/api/workouts/:id", (req, res) => {
-    db.Workout.findOne({_id:req.params.id})
-    .then(dbWorkout => {
-        console.log(dbWorkout);
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-  });
-
-// "/api/workouts/range"
-// app.get("/api/workouts/range", (_, res) => {
-//     db.Workout.find({})
-//     .populate("seeds")
-//       .then(dbWorkout => {
-//           console.log(dbWorkout);
-//         res.json(dbWorkout);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-//   });
 // Start the server
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
